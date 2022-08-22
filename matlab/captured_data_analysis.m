e281e4c3f9a9f33c54dd981f2bf3609d7fbc8638
HosseinBeheshti <h.beheshti92@gmail.com>
2022-08-22 23:04:32 +0430

    100 MB test passed

--- matlab/captured_data_analysis.m
+++ matlab/captured_data_analysis.m
@@ -18,12 +18,12 @@ max_length = min(length(header_start), length(footer_start));
 
 valid_packet_index = find(header_start(1:max_length) + 24 == footer_start(1:max_length));
 
-valid_header_end = header_start(2:end-2)' +7;
+valid_header_end = header_start(2:end-2) +5;
 header_end_diff = diff(valid_header_end);
-time_stamp = ts.*get_data_slice(data_input, valid_header_end + 1, 4);
+time_stamp = ts.*get_data_slice(data_input, valid_header_end + 1, 6);
 time_stamp_diff = diff(time_stamp);
 c_value = get_data_slice(data_input, valid_header_end + 5, 6);
 d_value = get_data_slice(data_input, valid_header_end + 10, 6);
 s_value = d_value ./ c_value;
-plot(s_value(2:end));
+plot(time_stamp,s_value);
 %%
